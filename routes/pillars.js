f9cef449e5c50cce522ba72c07028bb93481b3c1
Symon <ramossy@oregonstate.edu>
2020-03-12 17:32:56 -0700

    implement fixes to pillar, working copy

--- routes/pillars.js
+++ routes/pillars.js
@@ -6,18 +6,46 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
  // const { id } = req.query
- const body = {
-  "name":"John",
-  "age":30,
-  "cars": {
-    "car1":"Ford",
-    "car2":"BMW",
-    "car3":"Fiat"
-  }
- };
- console.log(body);
+ 
+ const body = [
+   {
+  "id": 1,
+  "title":"Healing at So and So",
+  "date": '11-20-2019',
+  "category": 'Healing',
+  'description': 'This was a healing at So and So, very much cool',
+  'verses': [
+    'John 3:16',
+    'Hebrews 4:16',
+    'Romans 12:1-2'
+  ]
+},
+{
+  "id": 2,
+  "title":"Miracle in Portland",
+  "date": '11-21-2019',
+  "category": 'Miracle',
+  'description': 'God did this amazing thing in this place',
+  'verses': [
+    '1 John 3:16',
+    'Hebrews 11:1',
+    'Colossians 3:17'
+  ]
+},
+ ];
+ 
+//const body = [['sdfd', 2, 'dsfds'], ['fgdfgfd', 45, 'ghffgf']];
 
- res.status(200).json(body);
+  res.status(200).json(body);
+
+ //console.log(body);
+
+ /*
+  Promise.all(promises).then(() => {
+  res.status(200).json(lists)
+})
+*/
+ //res.status(200).json(body);
  
 
 });
